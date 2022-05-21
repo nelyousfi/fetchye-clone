d245a3148b2402db48071d4e4f8673210b4d3031
Naoufal Elyoussoufi <n.elyousfi@gmail.com>
2022-05-21 21:34:57 +0200

    Add fetchye provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { FetchyeProvider } from 'fetchye'
+
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <FetchyeProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </FetchyeProvider>
     </React.StrictMode>
 )
